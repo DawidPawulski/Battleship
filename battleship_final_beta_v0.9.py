e68a243bc5ec1c6e87c6db61eee31dee54051217
Dawid Pawulski <dpawulski@gmail.com>
2019-04-09 14:20:43 +0200

    Add vertical position for AI

--- battleship_final_beta_v0.9.py
+++ battleship_final_beta_v0.9.py
@@ -22,11 +22,12 @@ def board_drawing(board):
     k = 0
     while i < board_radical:
         j = 0
+        print(" | ", end=" ")
         while j < board_radical:
             if len(str(board[k])) == 1:
-                print(str(" {}".format(board[k])) + " | ", end=" ")
+                print(str("{}".format(board[k])).center(6) + " | ", end=" ")
             else:
-                print(str("{}".format(board[k])) + " | ", end=" ")
+                print(str("{}".format(board[k])).center(6) + " | ", end=" ")
             j += 1
             k += 1
         print("\n")
@@ -42,6 +43,8 @@ def setting_ships_position(board, ship_class, ship_lenght):
                 print(f"It must be {ship_lenght} values")
                 continue
             for n in position:
+                # boom = u"\U0001F4A5"
+                # ship = u"\U0001F6A2"
                 board[int(n)-1] = "+"
             return position
         except ValueError:
@@ -66,7 +69,7 @@ def setting_position_pl_1():
 
 def setting_position_pl_2():
     print("Player 2 please enter position of your ships")
-    
+
     global ships_4_position_player_2
     ships_4_position_player_2 = setting_ships_position(player_2_board_position, "aircraft carrier", "4")
 
@@ -79,39 +82,98 @@ def setting_position_pl_2():
 
 def setting_position_ai():
 
-    position_ships_4_list = [1, 2, 3, 7, 8, 9, 13, 14, 15, 19, 20, 21, 25, 26, 27, 31, 32, 33]
+    horizontal_or_vertical = [1, 0]
 
-    position_ship_3_list = [1, 2, 3, 4, 7, 8, 9, 10, 13, 14, 15, 16, 19, 20, 21, 22, 25, 26, 27,
-                            28, 31, 32, 33, 34]
+    position_ships_4_list_vert = [1, 7, 13, 2, 8, 14, 3, 9, 15, 4, 10, 16, 5, 11, 17, 6, 12, 18]
 
-    position_ship_2_list = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 19, 20, 21, 22,
-                            23, 25, 26, 27, 28, 29, 31, 32, 33, 34, 35]
+    position_ship_3_list_vert = [1, 7, 13, 2, 8, 14, 3, 9, 15, 4, 10, 16, 5, 11, 17, 6, 12, 18, 19, 20, 21, 22, 23, 24]
 
-    random_num = random.choice(position_ships_4_list)
-    ships_4_first_position = ai_random_shooting[random_num-1]
+    position_ship_2_list_vert = [1, 7, 13, 2, 8, 14, 3, 9, 15, 4, 10, 16, 5, 11, 17, 6, 12, 18, 19, 20, 21, 22, 23, 24,
+                                 25, 26, 27, 28, 29, 30]
+
+    position_ships_4_list_horiz = [1, 2, 3, 7, 8, 9, 13, 14, 15, 19, 20, 21, 25, 26, 27, 31, 32, 33]
+
+    position_ship_3_list_horiz = [1, 2, 3, 4, 7, 8, 9, 10, 13, 14, 15, 16, 19, 20, 21, 22, 25, 26, 27,
+                                  28, 31, 32, 33, 34]
+
+    position_ship_2_list_horiz = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 19, 20, 21, 22,
+                                  23, 25, 26, 27, 28, 29, 31, 32, 33, 34, 35]
+
+    random_direction_s4 = random.choice(horizontal_or_vertical)
+
     global ships_4_ai_position
-    ships_4_ai_position = [str(ships_4_first_position), str(ships_4_first_position+1), str(ships_4_first_position+2),
-                           str(ships_4_first_position+3)]
+    if random_direction_s4 == 1:
+        random_num = random.choice(position_ships_4_list_horiz)
+        ships_4_first_position = ai_random_shooting[random_num-1]
+        ships_4_ai_position = [str(ships_4_first_position), str(ships_4_first_position+1),
+                               str(ships_4_first_position+2), str(ships_4_first_position+3)]
+    elif random_direction_s4 == 0:
+        random_num = random.choice(position_ships_4_list_vert)
+        ships_4_first_position = ai_random_shooting[random_num-1]
+        ships_4_ai_position = [str(ships_4_first_position), str(ships_4_first_position+6),
+                               str(ships_4_first_position+12), str(ships_4_first_position+18)]
 
     for n in ships_4_ai_position:
-        if n in position_ship_3_list:
-            position_ship_3_list.remove(n)
-        if n in position_ship_2_list:
-            position_ship_2_list.remove(n)
+        if n in position_ship_3_list_horiz:
+            position_ship_3_list_horiz.remove(n)
+        if n in position_ship_3_list_vert:
+            position_ship_3_list_vert.remove(n)
+        if n in position_ship_2_list_horiz:
+            position_ship_2_list_horiz.remove(n)
+        if n in position_ship_2_list_vert:
+            position_ship_2_list_vert.remove(n)
 
-    random_num = random.choice(position_ship_3_list)
-    ship_3_first_position = ai_random_shooting[random_num-1]
-    global ship_3_ai_position
-    ship_3_ai_position = [str(ship_3_first_position), str(ship_3_first_position+1), str(ship_3_first_position+2)]
+    while True:
+        random_direction_s3 = random.choice(horizontal_or_vertical)
+        global ship_3_ai_position
+        if random_direction_s3 == 1:
+            random_num = random.choice(position_ship_3_list_horiz)
+            ship_3_first_position = ai_random_shooting[random_num-1]
+            ship_3_ai_position = [str(ship_3_first_position), str(ship_3_first_position+1),
+                                  str(ship_3_first_position+2)]
+            for n in ship_3_ai_position:
+                if n in ships_4_ai_position:
+                    continue
+            break
+        elif random_direction_s3 == 0:
+            random_num = random.choice(position_ship_3_list_vert)
+            ship_3_first_position = ai_random_shooting[random_num-1]
+            ship_3_ai_position = [str(ship_3_first_position), str(ship_3_first_position+6),
+                                  str(ship_3_first_position+12)]
+            for n in ship_3_ai_position:
+                if n in ships_4_ai_position:
+                    continue
+            break
 
     for n in ship_3_ai_position:
-        if n in position_ship_2_list:
-            position_ship_2_list.remove(n)
+        if n in position_ship_2_list_horiz:
+            position_ship_2_list_horiz.remove(n)
+        if n in position_ship_2_list_vert:
+            position_ship_2_list_vert.remove(n)
 
-    random_num = random.choice(position_ship_2_list)
-    ship_2_first_position = ai_random_shooting[random_num-1]
-    global ship_2_ai_position
-    ship_2_ai_position = [str(ship_2_first_position), str(ship_2_first_position+1)]
+    while True:
+        random_direction_s2 = random.choice(horizontal_or_vertical)
+        global ship_2_ai_position
+        if random_direction_s2 == 1:
+            random_num = random.choice(position_ship_2_list_horiz)
+            ship_2_first_position = ai_random_shooting[random_num-1]
+            ship_2_ai_position = [str(ship_2_first_position), str(ship_2_first_position+1)]
+            for n in ship_2_ai_position:
+                if n in ships_4_ai_position:
+                    continue
+                if n in ship_3_ai_position:
+                    continue
+            break
+        elif random_direction_s2 == 0:
+            random_num = random.choice(position_ship_2_list_vert)
+            ship_2_first_position = ai_random_shooting[random_num-1]
+            ship_2_ai_position = [str(ship_2_first_position), str(ship_2_first_position+6)]
+            for n in ship_2_ai_position:
+                if n in ships_4_ai_position:
+                    continue
+                if n in ship_3_ai_position:
+                    continue
+            break
 
     print("Computer picked position for his ships")
 
@@ -135,27 +197,28 @@ def players_shooting(which_player, opponents_board, score_board, ships_4, ship_3
             except ValueError:
                 print("Please write only integers!")
                 continue
-
+        hit = "X".center(6)
+        miss = "0".center(6)
         if shoot_board[i] in ships_4:
-            opponents_board[int(shoot_board[i])-1] = f"{Fore.GREEN} X{Style.RESET_ALL}"
+            opponents_board[int(shoot_board[i])-1] = f"{Fore.GREEN}{hit}{Style.RESET_ALL}"
             print("\n\n YOU HIT!")
             ships_4.remove(shoot_board[i])
             ships_sinked(ships_4, ship_3, ship_2)
             score_board = int(score_board)+1
         elif shoot_board[i] in ship_3:
-            opponents_board[int(shoot_board[i])-1] = f"{Fore.GREEN} X{Style.RESET_ALL}"
+            opponents_board[int(shoot_board[i])-1] = f"{Fore.GREEN}{hit}{Style.RESET_ALL}"
             print("\n\n YOU HIT!")
             ship_3.remove(shoot_board[i])
             ships_sinked(ships_4, ship_3, ship_2)
             score_board = int(score_board)+1
         elif shoot_board[i] in ship_2:
-            opponents_board[int(shoot_board[i])-1] = f"{Fore.GREEN} X{Style.RESET_ALL}"
+            opponents_board[int(shoot_board[i])-1] = f"{Fore.GREEN}{hit}{Style.RESET_ALL}"
             print("\n\n YOU HIT!")
             ship_2.remove(shoot_board[i])
             ships_sinked(ships_4, ship_3, ship_2)
             score_board = int(score_board)+1
         else:
-            opponents_board[int(shoot_board[i])-1] = f"{Fore.RED} 0{Style.RESET_ALL}"
+            opponents_board[int(shoot_board[i])-1] = f"{Fore.RED}{miss}{Style.RESET_ALL}"
             print("\n\n YOU MISSED")
         board_drawing(opponents_board)
         time.sleep(1)
@@ -199,18 +262,19 @@ def two_players():
     score_player_2 = 0
 
     while True:
-        
+
         score_player_1 = players_shooting("1", player_2_board, score_player_1, ships_4_position_player_2,
-                         ship_3_position_player_2, ship_2_position_player_2)
+                                          ship_3_position_player_2, ship_2_position_player_2)
 
         if int(score_player_1) == 9:
             break
 
         score_player_2 = players_shooting("2", player_1_board, score_player_2, ships_4_position_player_1,
-                         ship_3_position_player_1, ship_2_position_player_1)
+                                          ship_3_position_player_1, ship_2_position_player_1)
 
         if int(score_player_2) == 9:
             break
+
 
 def play_against_ai():
 
@@ -228,7 +292,7 @@ def play_against_ai():
 
     while True:
         score_player_1 = players_shooting("1", ai_board, score_player_1, ships_4_ai_position,
-                         ship_3_ai_position, ship_2_ai_position)
+                                          ship_3_ai_position, ship_2_ai_position)
 
         if int(score_player_1) == 9:
             break
@@ -243,27 +307,28 @@ def play_against_ai():
             ai_random_shooting.remove(ai_shoot)
             ai_shoot_list.append(ai_shoot)
             print("AI shooted at position {}".format(ai_shoot))
-
+            hit = "X".center(6)
+            miss = "0".center(6)
             if str(ai_shoot_list[j]) in ships_4_position_player_1:
-                player_1_board[int(ai_shoot_list[j])-1] = f"{Fore.GREEN} X{Style.RESET_ALL}"
+                player_1_board[int(ai_shoot_list[j])-1] = f"{Fore.GREEN}{hit}{Style.RESET_ALL}"
                 ships_4_position_player_1.remove(str(ai_shoot_list[j]))
                 ships_sinked(ships_4_position_player_1, ship_3_position_player_1, ship_2_position_player_1)
                 print("\n AI HIT! \n")
                 score_ai = int(score_ai)+1
             elif str(ai_shoot_list[j]) in ship_3_position_player_1:
-                player_1_board[int(ai_shoot_list[j])-1] = f"{Fore.GREEN} X{Style.RESET_ALL}"
+                player_1_board[int(ai_shoot_list[j])-1] = f"{Fore.GREEN}{hit}{Style.RESET_ALL}"
                 ship_3_position_player_1.remove(str(ai_shoot_list[j]))
                 ships_sinked(ships_4_position_player_1, ship_3_position_player_1, ship_2_position_player_1)
                 print("\n AI HIT! \n")
                 score_ai = int(score_ai)+1
             elif str(ai_shoot_list[j]) in ship_2_position_player_1:
-                player_1_board[int(ai_shoot_list[j])-1] = f"{Fore.GREEN} X{Style.RESET_ALL}"
+                player_1_board[int(ai_shoot_list[j])-1] = f"{Fore.GREEN}{hit}{Style.RESET_ALL}"
                 ship_2_position_player_1.remove(str(ai_shoot_list[j]))
                 ships_sinked(ships_4_position_player_1, ship_3_position_player_1, ship_2_position_player_1)
                 print("\n AI HIT! \n")
                 score_ai = int(score_ai)+1
             else:
-                player_1_board[int(ai_shoot_list[j])-1] = f"{Fore.RED} 0{Style.RESET_ALL}"
+                player_1_board[int(ai_shoot_list[j])-1] = f"{Fore.RED}{miss}{Style.RESET_ALL}"
                 print("\n AI MISSED \n")
             board_drawing(player_1_board)
             time.sleep(4)
